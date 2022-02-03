7a8e7fa220d86a0fb42c8bd3b55d895f4d54a3a8
Alessandro Procopio <aledev21@gmail.com>
2022-02-03 01:28:28 -0300

    create params route

--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ const app = express()
 
 app.use(express.json())
 
+const customers = []
+
  
 app.post("/account", (req, res) =>{
     const {cpf, name} = req.body;
 
     const id = uuidv4()
 
-    
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
 
-    return res.json({message: 'nodejs fundatations'})
+
+    return res.status(201).send()
 })
 app.listen(3000)
