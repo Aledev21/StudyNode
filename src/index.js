ff5ca1a83fddb0aaa4e70cd1833f2b41d06b8994
Alessandro Procopio <aledev21@gmail.com>
2022-02-03 01:24:41 -0300

    create router cadastro and install uuid

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const {v4: uuidv4} = require('uuid')
+
 
 const app = express()
 
 app.use(express.json())
 
  
-app.get("/", (req, res) =>{
+app.post("/account", (req, res) =>{
+    const {cpf, name} = req.body;
+
+    const id = uuidv4()
+
+    
+
     return res.json({message: 'nodejs fundatations'})
 })
 app.listen(3000)
