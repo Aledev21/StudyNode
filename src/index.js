cfd07b4e3bffaf432a34d94b59751e41c7ae48b3
Alessandro Procopio <aledev21@gmail.com>
2022-02-03 00:39:25 -0300

    create examples routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) =>{
-    return res.json({message: 'hello world'})
+    return res.json({message: 'nodejs fundatations'})
 })
+
+app.get("/courses", ( req, res) => {
+    return res.json(['CURSO1', 'CURSO2', 'CURSO3' ])
+})
+
+app.post("/courses", ( req, res) => {
+    return res.json(['CURSO1', 'CURSO2', 'CURSO3', 'CURSO4' ])
+})
+
+app.post("/courses/:id", ( req, res) => {
+    return res.json(['CURSO7', 'CURSO2', 'CURSO3', 'CURSO4' ])
+})
+
+app.patch("/courses/:id", (req, res)=>{
+    return res.json(['CURSO7', 'CURSO6', 'CURSO3', 'CURSO4' ])
+
+} )
 
 
 app.listen(3000)
